a46e2ccbb3ed1f6f1152bb3b5e52fe3a46bd3599
Mudy Mud <bastien.masselon@laposte.net>
2025-03-10 17:14:59 +0100

    Configuration de Spring Secutiry et inscription de l'utilisateur si son email n'existe pas déjà

---
+++ src/main/java/org/wild/myblog/dto/user/UserRegistrationDTO.java
@@ -0,0 +1,30 @@
+package org.wild.myblog.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserRegistrationDTO {
+
+    @NotBlank
+    @Email(message = "L'email doit être valide")
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
