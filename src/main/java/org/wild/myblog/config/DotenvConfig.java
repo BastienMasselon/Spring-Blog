c415d3d0133195681d20542d6ed43fe08a1f5308
Mudy Mud <bastien.masselon@laposte.net>
2025-09-25 11:07:54 +0200

    Docker & Docker compose ; does not work on local without docker compose (dotenv missing)

--- src/main/java/org/wild/myblog/config/DotenvConfig.java
+++ src/main/java/org/wild/myblog/config/DotenvConfig.java
@@ -8,15 +8,15 @@ import org.springframework.core.env.MapPropertySource;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DotenvConfig implements EnvironmentPostProcessor {
-    @Override
-    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        // Charger le fichier .env
-        Dotenv dotenv = Dotenv.load();
-        // Stocker les variables du .env dans un map
-        Map<String, Object> dotenvProperties = new HashMap<>();
-        dotenv.entries().forEach(entry -> dotenvProperties.put(entry.getKey(), entry.getValue()));
-        // Ajouter les propriétés au contexte Spring
-        environment.getPropertySources().addFirst(new MapPropertySource("dotenvProperties", dotenvProperties));
-    }
-}
+//public class DotenvConfig implements EnvironmentPostProcessor {
+//    @Override
+//    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+//        // Charger le fichier .env
+//        Dotenv dotenv = Dotenv.load();
+//        // Stocker les variables du .env dans un map
+//        Map<String, Object> dotenvProperties = new HashMap<>();
+//        dotenv.entries().forEach(entry -> dotenvProperties.put(entry.getKey(), entry.getValue()));
+//        // Ajouter les propriétés au contexte Spring
+//        environment.getPropertySources().addFirst(new MapPropertySource("dotenvProperties", dotenvProperties));
+//    }
+//}
